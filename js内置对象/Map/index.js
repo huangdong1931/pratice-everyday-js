cdefb53454c33267e57300e8370d31c92e51f96a
黄东 <DHHD1931@163.COM>
2022-08-14 04:22:31 +0800

    Set

--- js内置对象/Map/index.js
+++ js内置对象/Map/index.js
@@ -25,19 +25,22 @@ let list = [
   },
   {
     id: 3,
-    value: 'map3'
+    value: 'map4'
   }
 ]
 for (let key of list) {
+  // set方法返回map对象，所以可以链式调用
   map.set(key.id, key)
 }
+
+console.log('map:', map.get(3))
 
 // 将map转化为list
 let c_list = [...map.values()]
 
 // 获取map所有的健
 let k_list = [...map.keys()]
-// 总结：对象有唯一主键区分，Map可用于数组对象去重，由于Map的健是唯一性，相同的健将不会参与后续插入动作
+// 总结：对象有唯一主键区分，Map可用于数组对象去重，由于Map的健是唯一性，相同的健将会覆盖之前的值
 
 // 判断数组中的主键，map是否有一个存在
 console.log([1, 2, 3].some(it => map.has(it)))
