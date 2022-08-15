c2b66efca584b78f6e7435efd90e3cf9dcae5df7
黄东 <DHHD1931@163.COM>
2022-08-15 23:10:58 +0800

    proxy

--- js内置对象/Proxy/index.js
+++ js内置对象/Proxy/index.js
@@ -5,9 +5,9 @@
 
 /**
  * 当handler为get方法时：方法用于拦截对象的读取属性操作
- * target：目标对象
- * property：被获取的属性名
- * receiver：Proxy 或者继承 Proxy 的对象
+ * @param target：目标对象
+ * @param property：被获取的属性名
+ * @param receiver：Proxy 或者继承 Proxy 的对象
  * @return get 方法可以返回任何值
  */
 let proxy = new Proxy({a: 4}, {
@@ -17,3 +17,35 @@ let proxy = new Proxy({a: 4}, {
   }
 })
 console.log('proxy:', proxy)
+
+/**
+ * 当handler为set方法时：方法用于设置属性值操作的捕获器
+ * @param target 目标对象
+ * @param property 将被设置的属性名或 Symbol
+ * @param target 新属性值
+ * @param receiver 最初被调用的对象。通常是 proxy 本身，但 handler 的 set 方法也有可能在原型链上，或以其他方式被间接地调用（因此不一定是 proxy 本身）
+ * @return set() 方法应当返回一个布尔值 返回 true 代表属性设置成功 在严格模式下，如果 set() 方法返回 false，那么会抛出一个 TypeError 异常
+ */
+
+let valid = {
+  set: function(obj, prop, value) {
+    if (prop === 'age') {
+      // 如果不是整数则抛出异常
+      if (!Number.isInteger(value)) {
+        throw new Error('年龄必须为整数')
+      }
+    }
+    obj[prop] = value
+  }
+}
+let proxy1 = new Proxy({}, valid)
+
+
+let HTTP = {
+  GET: (url, params) => {
+    return fetch(url).then(async response => await response.json() )
+  },
+  POST: (url, params) => {}
+}
+
+console.log(HTTP.GET('./text.json'))
