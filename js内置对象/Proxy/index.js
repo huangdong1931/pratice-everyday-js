68e2d5329980c1d28eac8ca02da9c30f4cd03079
黄东 <DHHD1931@163.COM>
2022-08-17 04:14:27 +0800

    proxy

--- js内置对象/Proxy/index.js
+++ js内置对象/Proxy/index.js
@@ -10,13 +10,13 @@
  * @param receiver：Proxy 或者继承 Proxy 的对象
  * @return get 方法可以返回任何值
  */
-let proxy = new Proxy({a: 4}, {
-  get: (target, prop, receiver) => {
-    console.log('get:', target, receiver)
+let t1 = { a: 4 }
+let h1 = {
+  get(target, prop, receiver) {
     return `拦截${prop}`
   }
-})
-console.log('proxy:', proxy)
+}
+let p1 = new Proxy(t1, h1)
 
 /**
  * 当handler为set方法时：方法用于设置属性值操作的捕获器
@@ -26,8 +26,8 @@ console.log('proxy:', proxy)
  * @param receiver 最初被调用的对象。通常是 proxy 本身，但 handler 的 set 方法也有可能在原型链上，或以其他方式被间接地调用（因此不一定是 proxy 本身）
  * @return set() 方法应当返回一个布尔值 返回 true 代表属性设置成功 在严格模式下，如果 set() 方法返回 false，那么会抛出一个 TypeError 异常
  */
-
-let valid = {
+let t2 = {}
+let h2 = {
   set: function(obj, prop, value) {
     if (prop === 'age') {
       // 如果不是整数则抛出异常
@@ -38,17 +38,7 @@ let valid = {
     obj[prop] = value
   }
 }
-let proxy1 = new Proxy({}, valid)
-
-
-let HTTP = {
-  GET: (url, params) => {
-    return fetch(url).then(response => response.json())
-  },
-  POST: (url, params) => {}
-}
-
-console.log(HTTP.GET('./text.json'))
+let p2 = new Proxy(t2, h2)
 
 /**
  * 当handler.apply() 方法用于拦截函数的调用
@@ -58,29 +48,28 @@ console.log(HTTP.GET('./text.json'))
  * @return apply 方法可以返回任何值
  */
 
-let sum = (a, b) => a + b
-let handler = {
+let t3 = (a, b) => a + b
+let h3 = {
   apply: function(target, thisArg, argList) {
     return target(argList[0], argList[1]) * 10
   }
 }
-let proxy2 = new Proxy(sum, handler)
+let p3 = new Proxy(t3, h3)
 
-// 思考：apply是否可用于实现重复请求？
-let fn = (...arg) => {
-  return setTimeout(() => {
-    return arg
-  }, 2000)
+/**
+ * 当handler.has() 方法可以看作是针对 in 操作的钩子
+ * @param target 目标对象（函数）
+ * @param prop 需要检查是否存在的属性
+ * @return has 方法返回一个 boolean 属性的值
+ */
+
+let t4 = {
+  name: 'zs',
+  age: 18
 }
-let handlerFn = {
-  apply: function(t, ctx, argList) {
-    // 这里为什么会返回 1,2,3
-    console.log('apply:', t(...argList))
-    console.log('apply:', t(...argList))
-    console.log('apply:', t(...argList))
-    return t(...argList)
+let h4 = {
+  has(target, key) {
+    return key in target
   }
 }
-let proxy3 = new Proxy(fn, handlerFn)
-
-console.log('value:', proxy3(999, 8888))
+let p4 = new Proxy(t4, h4)
